cf5e44696f5c3291f8117f515130e97014b65dad
dehwyy <dehwyy@yandex.ru>
2024-09-22 15:57:08 +0300

    [FEAT] `ansidef_function` macros!

--- src/core/io/ansidef.rs
+++ src/core/io/ansidef.rs
@@ -1,18 +1,43 @@
-macro_rules! ansidef {
+macro_rules! ansidef_variable {
   ($var:ident, $val:literal) => {
     pub(super) const $var: u8 = $val;
   }
 }
 
-// Global
-ansidef!(RESET, 0);
 
+macro_rules! ansidef_function {
+    ($var:ident, $fn:expr, $args:expr) => {
+        pub fn $var<Iter: IntoIterator<Item = impl std::fmt::Display>>(args: Iter) -> Vec<String> {
+          $args.into_iter().map(|s| s.to_string())
+            .chain(
+              args.into_iter().map(|s| s.to_string())
+            )
+          .collect::<Vec<_>>()
+        }
+    };
+    ($var:ident, $fn:expr) => {
+      ansidef_function!($var, $fn, [] as [u8; 0]);
+    }
+}
+
+// Global
+ansidef_variable!(RESET, 0x0);
 // Style
-ansidef!(BOLD, 1);
-ansidef!(UNDERLINE, 4);
+ansidef_variable!(BOLD, 0x1);
+ansidef_variable!(DIM, 0x2);
+ansidef_variable!(ITALIC, 0x3);
+ansidef_variable!(UNDERLINE, 0x4);
+ansidef_variable!(BLINKING, 0x5);
+ansidef_variable!(INVERTED, 0x7);
+ansidef_variable!(HIDDEN, 0x8);
+ansidef_variable!(STRIKETHROUGH, 0x9);
 
 // Color
-ansidef!(RED, 31);
-ansidef!(GREEN, 32);
-ansidef!(BLUE, 34);
-ansidef!(WHITE, 37);
+ansidef_variable!(BLACK, 0x30);
+ansidef_variable!(RED, 0x31);
+ansidef_variable!(GREEN, 0x32);
+ansidef_variable!(YELLOW, 0x33);
+ansidef_variable!(BLUE, 0x34);
+ansidef_variable!(MAGENTA, 0x35);
+ansidef_variable!(CYAN, 0x36);
+ansidef_variable!(WHITE, 0x37);
