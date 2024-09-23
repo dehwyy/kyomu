c7a044d63017a9443156297781558fea962bff34
dehwyy <dehwyy@yandex.ru>
2024-09-23 20:24:32 +0300

    [FEAT] new `OutputFlags` members

--- src/core/io/out_flags.rs
+++ src/core/io/out_flags.rs
@@ -2,9 +2,13 @@ bitflags::bitflags! {
   #[derive(Clone, Copy)]
   pub struct OutputFlags: u8 {
     const BOLD = 1;
-    const UNDERLINE = 1 << 1;
-    const CLEAR_LINE = 1 << 2;
-    const CLEAR_ALL = 1 << 3;
+    const DIM = 1 << 1;
+    const ITALIC = 1 << 2;
+    const UNDERLINE = 1 << 3;
+    const BLINKING = 1 << 4;
+    const INVERTED = 1 << 5;
+    const HIDDEN = 1 << 6;
+    const STRIKETHROUGH = 1 << 7;
   }
 }
 
@@ -15,18 +19,45 @@ impl OutputFlags {
     self
   }
 
+  pub fn dim(&mut self) -> &mut Self {
+    *self |= OutputFlags::DIM;
+    self
+  }
+
+  pub fn italic(&mut self) -> &mut Self {
+    *self |= OutputFlags::ITALIC;
+    self
+  }
+
   pub fn underline(&mut self) -> &mut Self {
     *self |= OutputFlags::UNDERLINE;
     self
   }
 
-  pub fn clear_line(&mut self) -> &mut Self {
-    *self |= OutputFlags::CLEAR_LINE;
+  pub fn blinking(&mut self) -> &mut Self {
+    *self |= OutputFlags::BLINKING;
     self
   }
 
-  pub fn clear_all(&mut self) -> &mut Self {
-    *self |= OutputFlags::CLEAR_ALL;
+  pub fn inverted(&mut self) -> &mut Self {
+    *self |= OutputFlags::INVERTED;
     self
+  }
+
+  pub fn hidden(&mut self) -> &mut Self {
+    *self |= OutputFlags::HIDDEN;
+    self
+  }
+
+  pub fn strikethrough(&mut self) -> &mut Self {
+    *self |= OutputFlags::STRIKETHROUGH;
+    self
+  }
+}
+
+
+impl Default for OutputFlags {
+  fn default() -> Self {
+    Self::empty()
   }
 }
