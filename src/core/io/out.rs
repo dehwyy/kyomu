e756fe95c3c94560694d0fe4ae39504ebd1341af
dehwyy <dehwyy@yandex.ru>
2024-09-23 20:26:24 +0300

    [FEAT] `io/out.rs` `bg_color`, `new_line`

--- src/core/io/out.rs
+++ src/core/io/out.rs
@@ -6,27 +6,32 @@ use super::ansi::AnsiSequence;
 use super::out_flags::OutputFlags;
 
 
-use crate::core::cell::color::Color;
+use crate::core::cell::color::{Color, Rgb};
 
 
+#[derive(Default)]
 pub struct Output {
   flags: OutputFlags,
-  color: Color,
+  fg_color: Color,
+  bg_color: Option<Color>,
+  new_line: bool
 }
 
-
-// static YELLOW: String = colored!(33);
 
 impl Output {
   pub fn new() -> Self {
     Self {
-      color: Color::default(),
-      flags: OutputFlags::empty(),
+      ..Default::default()
     }
   }
 
-  pub fn color(mut self, color: Color) -> Self {
-    self.color = color;
+  pub fn fg_color(mut self, color: Color) -> Self {
+    self.fg_color = color;
+    self
+  }
+
+  pub fn bg_color(mut self, color: Color) -> Self {
+    self.bg_color = Some(color);
     self
   }
 
@@ -35,16 +40,32 @@ impl Output {
     self
   }
 
+  // `false` by default
+  pub fn new_line(mut self, new_line: bool) -> Self {
+    self.new_line = new_line;
+    self
+  }
+
 
   pub async fn write<S>(&mut self, stdout: &mut Stdout, s: S) -> Result<(), WriteError>
   where S: Display
   {
-    let (ansi_start, ansi_end)  = AnsiSequence::new()
+    let mut ansi_sequence = AnsiSequence::new()
       .inject_flags(self.flags)
-      .inject_fg_color(self.color)
-      .compile();
+      .inject_fg_color(Color::Rgb(Rgb::new(200, 240, 10)));
 
-    let s = format!("{ansi_start}{s}{ansi_end}");
+    if let Some(bg_color) = self.bg_color {
+      ansi_sequence = ansi_sequence.inject_bg_color(bg_color);
+    }
+
+    let (ansi_start, ansi_end) = ansi_sequence.compile();
+
+
+    let mut s = format!("{ansi_start}{s}{ansi_end}");
+
+    if self.new_line {
+      s += "\n";
+    }
 
     stdout.write_all(s.as_bytes()).await?;
 
