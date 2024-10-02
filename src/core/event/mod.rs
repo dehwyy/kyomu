2c17d0fc9c9caceb98222921c432ec1b3a1d6479
dehwyy <dehwyy@yandex.ru>
2024-10-02 12:43:37 +0300

    [FIX] `Backspace` handle

--- src/core/event/mod.rs
+++ src/core/event/mod.rs
@@ -1,64 +1,57 @@
 pub mod key;
 
-use tokio::time::Duration;
-use crossterm::event::{Event as CrosstermEvent, poll as event_poll};
+use crossterm::event::{poll as event_poll, Event as CrosstermEvent};
 use key::{Key, KeyChar};
+use tokio::time::Duration;
 
 pub type EventReceiver = tokio::sync::broadcast::Receiver<Event>;
 
 #[derive(Debug, Clone)]
 pub enum Event {
-  Key(Key),
-  Quit
+    Key(Key),
+    Quit,
 }
 
 const ZERO_TIMEOUT: Duration = Duration::from_secs(0);
 
-
 impl Event {
-  /// Returns `Some(event)` if any event is available.
-  ///
-  /// `Some(EventQuit)`if error occuried;
-  ///
-  /// `None` if no event is available.
-  pub async fn read() -> Option<Self> {
-    match event_poll(ZERO_TIMEOUT) {
-      Ok(true) => {
-        let Ok(ev) = crossterm::event::read() else {
-          return Some(Self::Quit);
-        };
+    /// Returns `Some(event)` if any event is available.
+    ///
+    /// `Some(EventQuit)`if error occuried;
+    ///
+    /// `None` if no event is available.
+    pub async fn read() -> Option<Self> {
+        match event_poll(ZERO_TIMEOUT) {
+            Ok(true) => {
+                let Ok(ev) = crossterm::event::read() else {
+                    return Some(Self::Quit);
+                };
 
-        Some(ev.into())
-      },
-      Ok(_) => None,
-      Err(_) => Some(Self::Quit)
+                Some(ev.into())
+            }
+            Ok(_) => None,
+            Err(_) => Some(Self::Quit),
+        }
     }
-  }
 }
 
 impl From<CrosstermEvent> for Event {
-  fn from(event: CrosstermEvent) -> Self {
-    match event {
-      CrosstermEvent::Key(k) => {
-        let key_event = Key::from(k);
-        match &key_event {
-          Key::Backspace(modifiers) => {
-            // modifiers
-            todo!()
-          }
+    fn from(event: CrosstermEvent) -> Self {
+        match event {
+            CrosstermEvent::Key(k) => {
+                let key_event = Key::from(k);
+                match &key_event {
+                    Key::Char(key_char) => {
+                        if KeyChar::build('c').ctrl() == *key_char {
+                            return Self::Quit;
+                        }
 
-          Key::Char(key_char) => {
-            if KeyChar::build('c').ctrl() == *key_char {
-              return Self::Quit;
+                        Self::Key(key_event)
+                    }
+                    _key_ev => Self::Key(key_event),
+                }
             }
-
-            Self::Key(key_event)
-          },
-          _key_ev => Self::Key(key_event)
+            _ => Self::Quit,
         }
-      },
-      _ => Self::Quit
     }
-
-  }
 }
