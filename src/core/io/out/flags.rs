75d5d69f5928a8705b16335c47f2f0386ffcdebf
dehwyy <dehwyy@yandex.ru>
2024-10-04 00:00:34 +0300

    [REFORMAT] - core/io/out/flags.rs

--- src/core/io/out/flags.rs
+++ src/core/io/out/flags.rs
@@ -12,54 +12,52 @@ bitflags::bitflags! {
   }
 }
 
-
 impl OutputFlags {
-  pub fn bold(&mut self) -> &mut Self {
-    *self |= OutputFlags::BOLD;
-    self
-  }
+    pub fn bold(&mut self) -> &mut Self {
+        *self |= OutputFlags::BOLD;
+        self
+    }
 
-  pub fn dim(&mut self) -> &mut Self {
-    *self |= OutputFlags::DIM;
-    self
-  }
+    pub fn dim(&mut self) -> &mut Self {
+        *self |= OutputFlags::DIM;
+        self
+    }
 
-  pub fn italic(&mut self) -> &mut Self {
-    *self |= OutputFlags::ITALIC;
-    self
-  }
+    pub fn italic(&mut self) -> &mut Self {
+        *self |= OutputFlags::ITALIC;
+        self
+    }
 
-  pub fn underline(&mut self) -> &mut Self {
-    *self |= OutputFlags::UNDERLINE;
-    self
-  }
+    pub fn underline(&mut self) -> &mut Self {
+        *self |= OutputFlags::UNDERLINE;
+        self
+    }
 
-  pub fn blinking(&mut self) -> &mut Self {
-    *self |= OutputFlags::BLINKING;
-    self
-  }
+    pub fn blinking(&mut self) -> &mut Self {
+        *self |= OutputFlags::BLINKING;
+        self
+    }
 
-  pub fn inverted(&mut self) -> &mut Self {
-    *self |= OutputFlags::INVERTED;
-    self
-  }
+    pub fn inverted(&mut self) -> &mut Self {
+        *self |= OutputFlags::INVERTED;
+        self
+    }
 
-  pub fn hidden(&mut self) -> &mut Self {
-    *self |= OutputFlags::HIDDEN;
-    self
-  }
+    pub fn hidden(&mut self) -> &mut Self {
+        *self |= OutputFlags::HIDDEN;
+        self
+    }
 
-  pub fn strikethrough(&mut self) -> &mut Self {
-    *self |= OutputFlags::STRIKETHROUGH;
-    self
-  }
+    pub fn strikethrough(&mut self) -> &mut Self {
+        *self |= OutputFlags::STRIKETHROUGH;
+        self
+    }
 }
 
-
 impl Default for OutputFlags {
-  fn default() -> Self {
-    Self::empty()
-  }
+    fn default() -> Self {
+        Self::empty()
+    }
 }
 
 bitflags::bitflags! {
@@ -71,13 +69,13 @@ bitflags::bitflags! {
 }
 
 impl OutputGroupFlags {
-  pub fn new_lined(&mut self) -> &mut Self {
-    *self |= OutputGroupFlags::NEW_LINE;
-    self
-  }
+    pub fn new_line(&mut self) -> &mut Self {
+        *self |= OutputGroupFlags::NEW_LINE;
+        self
+    }
 
-  pub fn clear_line(&mut self) -> &mut Self {
-    *self |= OutputGroupFlags::CLEAR_LINE;
-    self
-  }
+    pub fn clear_line(&mut self) -> &mut Self {
+        *self |= OutputGroupFlags::CLEAR_LINE;
+        self
+    }
 }
