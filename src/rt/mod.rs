846ce57b7c303ebd6a4cb39341372f1cb7d8243d
dehwyy <dehwyy@yandex.ru>
2024-10-05 15:27:01 +0300

    [FEAT] `RuntimeConfig` singleton

--- src/rt/mod.rs
+++ src/rt/mod.rs
@@ -1,1 +1,11 @@
 pub mod config;
+
+use config::RuntimeConfig;
+use std::sync::OnceLock;
+use tokio::sync::RwLock;
+
+static RT_CONFIG: OnceLock<RwLock<RuntimeConfig>> = OnceLock::new();
+
+pub fn get_rt_config() -> &'static RwLock<RuntimeConfig> {
+    RT_CONFIG.get_or_init(|| RwLock::new(RuntimeConfig::new()))
+}
