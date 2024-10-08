c1a11cf0279e4f8e3dff256963ee308aa47b2ab0
dehwyy <dehwyy@yandex.ru>
2024-10-09 00:14:00 +0300

    [FIX] - clear surface

--- src/core/surface/mod.rs
+++ src/core/surface/mod.rs
@@ -1,6 +1,6 @@
 use tokio::io::{AsyncWriteExt, Stdout};
 
-use super::io::ansi::global::AnsiGlobal;
+use super::{io::ansi::global::AnsiGlobal, terminal::Terminal};
 
 async fn w(s: impl std::fmt::Display, stdout: &mut Stdout) {
     stdout.write_all(s.to_string().as_bytes()).await.unwrap();
@@ -9,6 +9,7 @@ pub struct Surface;
 
 impl Surface {
     pub async fn clear(stdout: &mut Stdout) {
+        w(AnsiGlobal::MoveToCell(Terminal::get_size()), stdout).await;
         w(AnsiGlobal::ClearScreen, stdout).await
     }
 
